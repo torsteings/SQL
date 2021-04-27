9970268c390b8573cbaf2af238678030a7d24928
ctbirds <ctbirdsas@gmail.com>
2021-04-27 18:35:22 +0200

    first commit

--- jobs/settings.py
+++ jobs/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +87,8 @@ DATABASES = {
     }
 }
 
-
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
